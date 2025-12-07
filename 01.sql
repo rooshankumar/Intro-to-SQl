379e7a1183009f6ba34e555306d97c77d1db3bb2
Roshan Kumar <isthisroshan@gmail.com>
2025-12-07 15:19:36 +0530

    01Q

---
+++ 01.sql
@@ -0,0 +1,5 @@
+# Q1. List the first 10 customers with their first name, last name, and country.
+
+select "First Name","Last Name", "Country" from customer
+order by "First Name","Last Name","Country" 
+limit 10;
